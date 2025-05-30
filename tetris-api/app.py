3298106e6d3fcea3639a57abb53d3c1a37694a26
TonyHallier <119819024+TonyHallier@users.noreply.github.com>
2025-05-31 00:00:50 +0200

    Fix app run for deployment

--- tetris-api/app.py
+++ tetris-api/app.py
@@ -42,4 +42,5 @@ def post_score():
     return jsonify({"message": "Score added"}), 201
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
